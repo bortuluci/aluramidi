f161dbe188f2a3aac755812a337e60cbaa096f48
Khauan Bortuluci <khauanbs@gmail.com>
2023-01-26 10:48:53 -0300

    otimizando laço de repetição while por for

--- main.js
+++ main.js
@@ -6,19 +6,13 @@ const listaDeTeclas = document.querySelectorAll(".tecla")
 
 let contador = 0
 
-while (contador < listaDeTeclas.length) {
+//     declaração do contador    --      condição    --   incrementa +1 e reatribui novo valor
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador]
-
   const instrumento = tecla.classList[1]
-
   const idAudio = `#som_${instrumento}` //template string
-  // console.log(idAudio)
 
   tecla.onclick = function () {
     tocaSom(idAudio)
   }
-
-  contador = contador + 1
-
-  // console.log(contador)
 }
